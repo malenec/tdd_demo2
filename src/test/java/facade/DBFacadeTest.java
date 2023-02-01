0ec9c0b374112bc7956d83a4a94106ab6b8db004
malenec <90346924+malenec@users.noreply.github.com>
2023-02-01 15:27:57 +0100

    updated test 1

--- src/test/java/facade/DBFacadeTest.java
+++ src/test/java/facade/DBFacadeTest.java
@@ -49,7 +49,7 @@ class DBFacadeTest {
 
     @Test
     public void test() throws SQLException {
-        System.out.println("Testing database connection");
+        System.out.println("Testing database connection to see first name");
         String sql = "SELECT fname FROM startcode_test.usertable";
         try (ResultSet set = con.prepareStatement(sql).executeQuery()) {
             set.next();
